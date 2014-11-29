36704603c06c7a2958af72bece72598f1117c461
Sergiu Bagrin <sergey.roxigo@gmail.com>
2014-11-29 12:29:50 +0200

    Tickect Extension is under development so we moved it to Dev branch

--- extensions/event-tickets/config.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
-
-$cfg = array();
-
-$cfg['allowed_tickets'] = array( 'paid', 'free', 'donation' );
--- extensions/event-tickets/includes/sub-includes.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
-
-$dir = dirname( __FILE__ );
-require_once $dir . '/option-types/tickets/class-fw-option-type-tickets.php';
