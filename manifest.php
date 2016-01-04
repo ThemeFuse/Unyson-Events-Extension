a36b24112a622a7bffcf6d8a7a1d5e4e67c28c1c
Alexei Gogu <llemurya@gmail.com>
2016-01-04 15:25:56 +0200

    change manifest version

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Events', 'fw' );
 $manifest['description'] = __( 'This extension adds a fully fledged Events module to your theme. It comes with built in pages that contain a calendar where events can be added.', 'fw' );
-$manifest['version'] = '1.0.8';
+$manifest['version'] = '1.0.9';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
