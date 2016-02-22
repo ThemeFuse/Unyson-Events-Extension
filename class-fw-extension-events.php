2a841fe0eacd842e3b47cc3093aba23ed4767880
George <pisareno.gh@gmail.com>
2016-02-22 20:45:37 +0200

    Code refactoring

--- class-fw-extension-events.php
+++ class-fw-extension-events.php
@@ -2,48 +2,41 @@
 	die( 'Forbidden' );
 }
 
-class FW_Extension_Events extends FW_Extension
-{
+class FW_Extension_Events extends FW_Extension {
 	private $post_type_name = 'fw-event';
 	private $post_type_slug = 'fw-event-slug';
-	private $taxonomy_name  = 'fw-event-taxonomy-name';
-	private $taxonomy_slug  = 'fw-event-taxonomy-slug';
+	private $taxonomy_name = 'fw-event-taxonomy-name';
+	private $taxonomy_slug = 'fw-event-taxonomy-slug';
 
 	/**
 	 * @var string main option key
 	 */
 	private $event_option_id = 'general-event';
 
-	public function get_event_option_id()
-	{
+	public function get_event_option_id() {
 		return $this->event_option_id;
 	}
 
-	public function fw_get_post_type_slug()
-	{
+	public function fw_get_post_type_slug() {
 		return $this->post_type_slug;
 	}
 
-	public function get_post_type_name()
-	{
+	public function get_post_type_name() {
 		return $this->post_type_name;
 	}
 
-	public function get_taxonomy_name()
-	{
+	public function get_taxonomy_name() {
 		return $this->taxonomy_name;
 	}
 
-	public function _get_link()
-	{
-		return self_admin_url('edit.php?post_type=' . $this->get_post_type_name());
+	public function _get_link() {
+		return self_admin_url( 'edit.php?post_type=' . $this->get_post_type_name() );
 	}
 
 	/**
 	 * @internal
 	 */
-	protected function _init()
-	{
+	protected function _init() {
 		$this->define_slugs();
 		$this->register_post_type();
 		$this->register_taxonomy();
@@ -56,66 +49,66 @@ class FW_Extension_Events extends FW_Extension
 		}
 	}
 
-	private function define_slugs()
-	{
+	private function define_slugs() {
 		$this->post_type_slug = apply_filters( 'fw_ext_' . $this->get_name() . '_post_slug', $this->post_type_slug );
 		$this->taxonomy_slug  = apply_filters( 'fw_ext_' . $this->get_name() . '_taxonomy_slug', $this->taxonomy_slug );
 	}
 
-	private function register_post_type()
-	{
-		$post_names = apply_filters( 'fw_ext_' . $this->get_name() . '_post_type_name', array(
-			'singular' => __( 'Event', 'fw' ),
-			'plural'   => __( 'Events', 'fw' )
-		) );
+	private function register_post_type() {
+		$post_names = apply_filters( 'fw_ext_' . $this->get_name() . '_post_type_name',
+			array(
+				'singular' => __( 'Event', 'fw' ),
+				'plural'   => __( 'Events', 'fw' )
+			) );
 
-		register_post_type( $this->post_type_name, array(
-			'labels'             => array(
-				'name'               => __( 'Events', 'fw' ),
-				'singular_name'      => __( 'Event', 'fw' ),
-				'add_new'            => __( 'Add New', 'fw' ),
-				'add_new_item'       => sprintf( __( 'Add New %s', 'fw' ), $post_names['singular'] ),
-				'edit'               => __( 'Edit', 'fw' ),
-				'edit_item'          => sprintf( __( 'Edit %s', 'fw' ), $post_names['singular'] ),
-				'new_item'           => sprintf( __( 'New %s', 'fw' ), $post_names['singular'] ),
-				'all_items'          => sprintf( __( 'All %s', 'fw' ), $post_names['plural'] ),
-				'view'               => sprintf( __( 'View %s', 'fw' ), $post_names['singular'] ),
-				'view_item'          => sprintf( __( 'View %s', 'fw' ), $post_names['singular'] ),
-				'search_items'       => sprintf( __( 'Search %s', 'fw' ), $post_names['plural'] ),
-				'not_found'          => sprintf( __( 'No %s Found', 'fw' ), $post_names['plural'] ),
-				'not_found_in_trash' => sprintf( __( 'No %s Found In Trash', 'fw' ), $post_names['plural'] ),
-				'parent_item_colon'  => '' /* text for parent types */
-			),
-			'description'        => __( 'Create a event item', 'fw' ),
-			'public'             => true,
-			'show_ui'            => true,
-			'show_in_menu'       => true,
-			'publicly_queryable' => true,
-			/* queries can be performed on the front end */
-			'has_archive'        => true,
-			'rewrite'            => array(
-				'slug' => $this->post_type_slug
-			),
-			'menu_position'      => 5,
-			'show_in_nav_menus'  => false,
-			'menu_icon'          => 'dashicons-calendar',
-			'hierarchical'       => false,
-			'query_var'          => true,
-			/* Sets the query_var key for this post type. Default: true - set to $post_type */
-			'supports'           => array(
-				'title', /* Text input field to create a post title. */
-				'editor',
-				'thumbnail', /* Displays a box for featured image. */
-			)
-		) );
+		register_post_type( $this->post_type_name,
+			array(
+				'labels'             => array(
+					'name'               => __( 'Events', 'fw' ),
+					'singular_name'      => __( 'Event', 'fw' ),
+					'add_new'            => __( 'Add New', 'fw' ),
+					'add_new_item'       => sprintf( __( 'Add New %s', 'fw' ), $post_names['singular'] ),
+					'edit'               => __( 'Edit', 'fw' ),
+					'edit_item'          => sprintf( __( 'Edit %s', 'fw' ), $post_names['singular'] ),
+					'new_item'           => sprintf( __( 'New %s', 'fw' ), $post_names['singular'] ),
+					'all_items'          => sprintf( __( 'All %s', 'fw' ), $post_names['plural'] ),
+					'view'               => sprintf( __( 'View %s', 'fw' ), $post_names['singular'] ),
+					'view_item'          => sprintf( __( 'View %s', 'fw' ), $post_names['singular'] ),
+					'search_items'       => sprintf( __( 'Search %s', 'fw' ), $post_names['plural'] ),
+					'not_found'          => sprintf( __( 'No %s Found', 'fw' ), $post_names['plural'] ),
+					'not_found_in_trash' => sprintf( __( 'No %s Found In Trash', 'fw' ), $post_names['plural'] ),
+					'parent_item_colon'  => '' /* text for parent types */
+				),
+				'description'        => __( 'Create a event item', 'fw' ),
+				'public'             => true,
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+				'publicly_queryable' => true,
+				/* queries can be performed on the front end */
+				'has_archive'        => true,
+				'rewrite'            => array(
+					'slug' => $this->post_type_slug
+				),
+				'menu_position'      => 5,
+				'show_in_nav_menus'  => false,
+				'menu_icon'          => 'dashicons-calendar',
+				'hierarchical'       => false,
+				'query_var'          => true,
+				/* Sets the query_var key for this post type. Default: true - set to $post_type */
+				'supports'           => array(
+					'title', /* Text input field to create a post title. */
+					'editor',
+					'thumbnail', /* Displays a box for featured image. */
+				)
+			) );
 	}
 
-	private function register_taxonomy()
-	{
-		$category_names = apply_filters( 'fw_ext_' . $this->get_name() . '_category_name', array(
-			'singular' => __( 'Category', 'fw' ),
-			'plural'   => __( 'Categories', 'fw' )
-		) );
+	private function register_taxonomy() {
+		$category_names = apply_filters( 'fw_ext_' . $this->get_name() . '_category_name',
+			array(
+				'singular' => __( 'Category', 'fw' ),
+				'plural'   => __( 'Categories', 'fw' )
+			) );
 
 		$labels = array(
 			'name'              => sprintf( _x( 'Event %s', 'taxonomy general name', 'fw' ),
@@ -150,28 +143,29 @@ class FW_Extension_Events extends FW_Extension
 		register_taxonomy( $this->taxonomy_name, esc_attr( $this->post_type_name ), $args );
 	}
 
-	private function add_admin_filters()
-	{
+	private function add_admin_filters() {
 		add_filter(
 			'manage_' . $this->get_post_type_name() . '_posts_columns',
-			array( $this, '_filter_add_columns' ), 10, 1
+			array( $this, '_filter_add_columns' ),
+			10,
+			1
 		);
 		add_filter( 'fw_post_options', array( $this, '_filter_fw_post_options' ), 10, 2 );
-		add_filter( 'months_dropdown_results', array($this, '_filter_months_dropdown_results'));
+		add_filter( 'months_dropdown_results', array( $this, '_filter_months_dropdown_results' ) );
 	}
 
-	private function add_admin_actions()
-	{
+	private function add_admin_actions() {
 		add_action(
 			'manage_' . $this->get_post_type_name() . '_posts_custom_column',
-			array( $this, '_action_manage_custom_column' ), 10, 2
+			array( $this, '_action_manage_custom_column' ),
+			10,
+			2
 		);
 		add_action( 'admin_enqueue_scripts', array( $this, '_action_enqueue_scripts' ) );
 		add_action( 'admin_head', array( $this, '_action_initial_nav_menu_meta_boxes' ), 999 );
 	}
 
-	private function add_theme_actions()
-	{
+	private function add_theme_actions() {
 		add_action( 'wp', array( $this, '_action_calendar_export' ) );
 	}
 
@@ -181,11 +175,13 @@ class FW_Extension_Events extends FW_Extension
 	 * @internal
 	 */
 	public function _filter_add_columns( $columns ) {
-		unset($columns['date'], $columns['taxonomy-'.$this->taxonomy_name]);
-		return array_merge( $columns, array(
-			'event_date'     => __( 'Date', 'fw' ),
-			'event_location' => __( 'Location', 'fw' )
-		));
+		unset( $columns['date'], $columns[ 'taxonomy-' . $this->taxonomy_name ] );
+
+		return array_merge( $columns,
+			array(
+				'event_date'     => __( 'Date', 'fw' ),
+				'event_location' => __( 'Location', 'fw' )
+			) );
 	}
 
 	/**
@@ -193,27 +189,27 @@ class FW_Extension_Events extends FW_Extension
 	 *
 	 * @internal
 	 */
-	public function _filter_fw_post_options( $post_options, $post_type )
-	{
+	public function _filter_fw_post_options( $post_options, $post_type ) {
 		if ( $post_type !== $this->post_type_name ) {
 			return $post_options;
 		}
 
-		$event_options = apply_filters('fw_ext_events_post_options', array(
-			'events_tab' => array(
-				'title'   => __( 'Event Options', 'fw' ),
-				'type'    => 'tab',
-				'options' => array(
-					$this->event_option_id => array(
-						'type'  => 'event',
-						'desc'  => false,
-						'label' => false,
+		$event_options = apply_filters( 'fw_ext_events_post_options',
+			array(
+				'events_tab' => array(
+					'title'   => __( 'Event Options', 'fw' ),
+					'type'    => 'tab',
+					'options' => array(
+						$this->event_option_id => array(
+							'type'  => 'event',
+							'desc'  => false,
+							'label' => false,
+						)
 					)
 				)
-			)
-		));
+			) );
 
-		if ( isset($post_options['man']) && $post_options['main']['type'] === 'box' ) {
+		if ( isset( $post_options['man'] ) && $post_options['main']['type'] === 'box' ) {
 			$post_options['main']['options'][] = $event_options;
 		} else {
 			$post_options['main'] = array(
@@ -227,8 +223,7 @@ class FW_Extension_Events extends FW_Extension
 		return $post_options;
 	}
 
-	public function _filter_months_dropdown_results($months)
-	{
+	public function _filter_months_dropdown_results( $months ) {
 		$current_screen = array(
 			'only' => array(
 				array( 'post_type' => $this->post_type_name )
@@ -260,11 +255,12 @@ class FW_Extension_Events extends FW_Extension
 	 * Get saved event location array from db
 	 *
 	 * @param $post_id
+	 *
 	 * @return string
 	 */
-	private function get_event_location( $post_id )
-	{
+	private function get_event_location( $post_id ) {
 		$meta = fw_get_db_post_option( $post_id, $this->event_option_id );
+
 		return ( ( isset( $meta['event_location']['location'] ) and false === empty( $meta['event_location']['location'] ) ) ? $meta['event_location']['location'] : '&#8212;' );
 	}
 
@@ -275,8 +271,7 @@ class FW_Extension_Events extends FW_Extension
 	 *
 	 * @return string
 	 */
-	private function get_event_datetime_date( $post_id )
-	{
+	private function get_event_datetime_date( $post_id ) {
 		$meta      = fw_get_db_post_option( $post_id, $this->event_option_id );
 		$empty_msg = '&#8212;';
 
@@ -322,8 +317,7 @@ class FW_Extension_Events extends FW_Extension
 	 *
 	 * @internal
 	 */
-	public function _action_enqueue_scripts()
-	{
+	public function _action_enqueue_scripts() {
 		$current_screen = array(
 			'only' => array(
 				array( 'post_type' => $this->post_type_name )
@@ -342,8 +336,7 @@ class FW_Extension_Events extends FW_Extension
 	/**
 	 * @internal
 	 */
-	public function _action_initial_nav_menu_meta_boxes()
-	{
+	public function _action_initial_nav_menu_meta_boxes() {
 		$screen = array(
 			'only' => array(
 				'base' => 'nav-menus'
@@ -382,8 +375,7 @@ class FW_Extension_Events extends FW_Extension
 	/**
 	 * @intenral
 	 */
-	public function _action_calendar_export()
-	{
+	public function _action_calendar_export() {
 		global $post;
 		if ( empty( $post ) or $post->post_type !== $this->post_type_name ) {
 			return;
@@ -415,8 +407,7 @@ class FW_Extension_Events extends FW_Extension
 		}
 	}
 
-	private function get_google_uri( $post, $options, $row_id, $offset )
-	{
+	private function get_google_uri( $post, $options, $row_id, $offset ) {
 		$all_day = fw_akg( 'all_day', $options, 'yes' );
 
 		$date_template = 'Ymd';
@@ -431,21 +422,19 @@ class FW_Extension_Events extends FW_Extension
 		$location = fw_akg( 'event_location/location', $options, '' );
 
 		return 'https://www.google.com/calendar/render?action=TEMPLATE&text=' . $post->post_title .
-		'&dates=' . $start . '/' . $end .
-		'&details=For+details,+link+here:+' . get_permalink( $post->ID ) .
-		'&location=' . $location;
+		       '&dates=' . $start . '/' . $end .
+		       '&details=For+details,+link+here:+' . get_permalink( $post->ID ) .
+		       '&location=' . $location;
 	}
 
-	private function get_ics_headers( $post )
-	{
+	private function get_ics_headers( $post ) {
 		header( 'Content-type: text/calendar' );
 		header( 'Content-Disposition: attachment; filename=' . urlencode( $post->post_title ) . '-' . time() . '.ics' );
 		header( 'Pragma: no-cache' );
 		header( 'Expires: 0' );
 	}
 
-	private function get_ics_content( $post, $options, $row_id, $offset )
-	{
+	private function get_ics_content( $post, $options, $row_id, $offset ) {
 		$all_day = fw_akg( 'all_day', $options, 'yes' );
 
 		$date_template = 'Ymd\T000000';
@@ -460,15 +449,15 @@ class FW_Extension_Events extends FW_Extension
 		$location = fw_akg( 'event_location/location', $options, '' );
 
 		return "BEGIN:VCALENDAR\n" .
-		"VERSION:1.0\n" .
-		"BEGIN:VEVENT\n" .
-		"URL:" . get_permalink( $post->ID ) . "\n" .
-		"DTSTART:" . $start . "\n" .
-		"DTEND:" . $end . "\n" .
-		"SUMMARY:" . $post->post_title . "\n" .
-		"DESCRIPTION:For details, click here:" . get_permalink( $post->ID ) . "\n" .
-		"LOCATION:" . $location . "\n" .
-		"END:VEVENT\n" .
-		"END:VCALENDAR";
+		       "VERSION:1.0\n" .
+		       "BEGIN:VEVENT\n" .
+		       "URL:" . get_permalink( $post->ID ) . "\n" .
+		       "DTSTART:" . $start . "\n" .
+		       "DTEND:" . $end . "\n" .
+		       "SUMMARY:" . $post->post_title . "\n" .
+		       "DESCRIPTION:For details, click here:" . get_permalink( $post->ID ) . "\n" .
+		       "LOCATION:" . $location . "\n" .
+		       "END:VEVENT\n" .
+		       "END:VCALENDAR";
 	}
 }
